48903f2a1b60ccdb266d419caa5fdcf048d86495
hasangbong <hasb927@gmail.com>
2020-09-14 19:37:51 +0900

    github login

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
@@ -7,16 +8,27 @@ import {
   logout,
   postLogin,
   getLogin,
+  githubLogin,
+  postGithubLogin,
 } from "../controllers/userController";
+import { onlyPrivate, onlyPublic } from "../middlewares";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogin
+);
 
 export default globalRouter;
